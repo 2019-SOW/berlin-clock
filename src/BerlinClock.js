007122860daaae21efabdb7a91e81b9565f6ca88
2019-SOW <2019.SOW.Q1@gmail.com>
2019-06-10 17:35:41 +0200

    Initial project setup for berlin clock

---
+++ src/BerlinClock.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class BerlinClock extends React.Component {
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+
+}
+
+export default BerlinClock;
